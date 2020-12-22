7ce420750d0b7f54316ebae4cf620626b629fe7a
banalna <judas.priest999@gmail.com>
2020-12-22 20:38:17 +0300

    add registrations in BeaconsServiceFactory

--- src/build/BeaconsServiceFactory.ts
+++ src/build/BeaconsServiceFactory.ts
@@ -6,6 +6,9 @@ import { BeaconsMemoryPersistence } from '../persistence/BeaconsMemoryPersistenc
 import { BeaconsFilePersistence } from '../persistence/BeaconsFilePersistence';
 import { BeaconsMongoDbPersistence } from '../persistence/BeaconsMongoDbPersistence';
 import { BeaconsCouchbasePersistence } from '../persistence/BeaconsCouchbasePersistence';
+import { BeaconsPostgresPersistence } from '../persistence/BeaconsPostgresPersistence'
+import { BeaconsJsonPostgresPersistence } from '../persistence/BeaconsJsonPostgresPersistence'
+import { BeaconsSqlServerPersistence } from '../persistence/BeaconsSqlServerPersistence'
 import { BeaconsController } from '../logic/BeaconsController';
 import { BeaconsCommandableHttpServiceV1 } from '../services/version1/BeaconsCommandableHttpServiceV1';
 import { BeaconsCommandableGrpcServiceV1 } from '../services/version1/BeaconsCommandableGrpcServiceV1';
@@ -16,6 +19,9 @@ export class BeaconsServiceFactory extends Factory{
     public static FilePersistenceDescriptor = new Descriptor('beacons', 'persistence', 'file', '*', '1.0');
     public static MongoDbPersistenceDescriptor = new Descriptor('beacons', 'persistence', 'mongodb', '*', '1.0');
     public static CouchbasePersistenceDescriptor = new Descriptor('beacons', 'persistence', 'couchbase', '*', '1.0');
+    public static BeaconsPostgresPersistence = new Descriptor('beacons', 'persistence', 'postgres', '*', '1.0');
+    public static BeaconsJsonPostgresPersistence = new Descriptor('beacons', 'persistence', 'memory', '*', '1.0');
+    public static BeaconsSqlServerPersistence = new Descriptor('beacons', 'persistence', 'sqlserver', '*', '1.0');
     public static ControllerDescriptor = new Descriptor('beacons', 'controller', 'default', '*', '1.0');
     public static CommandableHttpServiceV1Descriptor = new Descriptor('beacons', 'service', 'commandable-http', '*', '1.0');
     public static CommandableGrpcServiceV1Descriptor = new Descriptor('beacons', 'service', 'commandable-grpc', '*', '1.0');
@@ -28,6 +34,9 @@ export class BeaconsServiceFactory extends Factory{
         this.registerAsType(BeaconsServiceFactory.FilePersistenceDescriptor, BeaconsFilePersistence);
         this.registerAsType(BeaconsServiceFactory.MongoDbPersistenceDescriptor, BeaconsMongoDbPersistence);
         this.registerAsType(BeaconsServiceFactory.CouchbasePersistenceDescriptor, BeaconsCouchbasePersistence);
+        this.registerAsType(BeaconsServiceFactory.BeaconsPostgresPersistence, BeaconsPostgresPersistence);
+        this.registerAsType(BeaconsServiceFactory.BeaconsJsonPostgresPersistence, BeaconsJsonPostgresPersistence);
+        this.registerAsType(BeaconsServiceFactory.BeaconsSqlServerPersistence, BeaconsSqlServerPersistence);
         this.registerAsType(BeaconsServiceFactory.ControllerDescriptor, BeaconsController);
         this.registerAsType(BeaconsServiceFactory.CommandableHttpServiceV1Descriptor, BeaconsCommandableHttpServiceV1);
         this.registerAsType(BeaconsServiceFactory.CommandableGrpcServiceV1Descriptor, BeaconsCommandableGrpcServiceV1);
