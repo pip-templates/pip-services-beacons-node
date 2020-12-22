903e2a55be5698f4bd6f50c650220b9b7967f021
EgorNuzhnykh <nuzhnykhev@gmail.com>
2020-12-22 21:12:06 +0000

    project build by Travis CI [skip ci]

---
+++ obj/src/container/BeaconsLambdaFunction.d.ts
@@ -0,0 +1,6 @@
+/** @module container */
+import { CommandableLambdaFunction } from 'pip-services3-aws-node';
+export declare class BeaconsLambdaFunction extends CommandableLambdaFunction {
+    constructor();
+}
+export declare const handler: (event: any, context: any) => void;
