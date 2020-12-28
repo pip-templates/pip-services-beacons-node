2fdeb55a6770c6474fe34f7b2c1d6d557493f3ac
banalna <judas.priest999@gmail.com>
2020-12-28 22:12:44 +0300

    add Rest Client and tests

--- src/clients/version1/BeaconsRestClientV1.ts
+++ src/clients/version1/BeaconsRestClientV1.ts
@@ -24,56 +24,153 @@ export class BeaconsRestClientV1 extends RestClient implements IBeaconsClientV1 
     public getBeacons(correlationId: string, filter: FilterParams, paging: PagingParams,
         callback: (err: any, page: DataPage<BeaconV1>) => void) {
 
-        let time = this.instrument(correlationId, "beacons.get_beacons")
-        this.call(
-            'get', 
-            '/beacons',
-            correlationId,
-            {
-                filter: filter,
-                paging: paging
-            },
-            null,
-            (err, page) => {
-                time.endTiming();
-                if (err == null && page != null) {
-                    callback(err, page)
-                } else {
-                    callback(err, null);
-                }                    
-            }
-        );
-    }
-
-    // TODO: implement all methods
-    public getBeaconById(correlationId: string, beaconId: string,
-        callback: (err: any, beacon: BeaconV1) => void): void {
-
-    }
-
-    public getBeaconByUdi(correlationId: string, udi: string,
-        callback: (err: any, beacon: BeaconV1) => void): void {
-
+            let time = this.instrument(correlationId, "beacons.get_beacons")
+            this.call(
+                'get', 
+                '/beacons',
+                correlationId,
+                {
+                    beacons:{
+                        filter: filter,
+                        paging: paging
+                    }
+                    
+                },
+                null,
+                (err, page) => {
+                    time.endTiming();
+                    if (err == null && page != null) {
+                        callback(err, page)
+                    } else {
+                        callback(err, null);
+                    }                    
+                }
+            );
     }
 
     public calculatePosition(correlationId: string, siteId: string, udis: string[], 
         callback: (err: any, position: any) => void): void {
-
+            let time = this.instrument(correlationId, "beacons.calculate_position")
+            this.call(
+                'post', 
+                '/beacons/position',
+                correlationId,
+                null,
+                {
+                    udis: udis,
+                    site_id: siteId
+                },
+                (err, beacon) => {
+                    time.endTiming();
+                    if (err == null && beacon != null) {
+                        callback(err, beacon)
+                    } else {
+                        callback(err, null);
+                    }                    
+                }
+            );
     }
 
     public createBeacon(correlationId: string, beacon: BeaconV1,
         callback: (err: any, beacon: BeaconV1) => void): void {
 
+            let time = this.instrument(correlationId, "beacons.create_beacon")
+            this.call(
+                'post', 
+                '/beacons',
+                correlationId,
+                null,
+                beacon,
+                (err, createdBeacon) => {
+                    time.endTiming();
+                    if (err == null && createdBeacon != null) {
+                        callback(err, createdBeacon)
+                    } else {
+                        callback(err, null);
+                    }                    
+                }
+            );
     }
 
     public updateBeacon(correlationId: string, beacon: BeaconV1,
         callback: (err: any, beacon: BeaconV1) => void): void {
+            let time = this.instrument(correlationId, "beacons.update_beacon")
+            this.call(
+                'post', 
+                '/beacons',
+                correlationId,
+                null,
+                beacon,
+                (err, updatedBeacon) => {
+                    time.endTiming();
+                    if (err == null && updatedBeacon != null) {
+                        callback(err, updatedBeacon)
+                    } else {
+                        callback(err, null);
+                    }                    
+                }
+            );
+    }
 
+    public getBeaconById(correlationId: string, id: string,
+        callback: (err: any, beacon: BeaconV1) => void): void {
+
+            let time = this.instrument(correlationId, "beacons.get_beacon_by_id")
+            this.call(
+                'get', 
+                '/beacons/' + id,
+                correlationId,
+                null,
+                null,
+                (err, beacon) => {
+                    time.endTiming();
+                    if (err == null && beacon != null) {
+                        callback(err, beacon)
+                    } else {
+                        callback(err, null);
+                    }                    
+                }
+            );
+    }
+
+    public getBeaconByUdi(correlationId: string, udi: string,
+        callback: (err: any, beacon: BeaconV1) => void): void {
+            let time = this.instrument(correlationId, "beacons.get_beacon_by_udi")
+            this.call(
+                'get', 
+                '/beacons/udi/' + udi,
+                correlationId,
+                null,
+                null,
+                (err, beacon) => {
+                    time.endTiming();
+                    if (err == null && beacon != null) {
+                        callback(err, beacon)
+                    } else {
+                        callback(err, null);
+                    }                    
+                }
+            );
     }
 
     public deleteBeaconById(correlationId: string, beaconId: string,
         callback: (err: any, beacon: BeaconV1) => void): void {
-
+            let time = this.instrument(correlationId, "beacons.delete_beacon_by_id")
+            this.call(
+                'delete', 
+                '/beacons/' + beaconId,
+                correlationId,
+                null,
+                null,
+                (err, beacon) => {
+                    time.endTiming();
+                    if (err == null && beacon != null) {
+                        callback(err, beacon)
+                    } else {
+                        callback(err, null);
+                    }                    
+                }
+            );
     }
     
 }
--- test/clients/version1/BeaconsRestClientV1.test.ts
+++ test/clients/version1/BeaconsRestClientV1.test.ts
@@ -1,69 +1,69 @@
-// import { Descriptor } from 'pip-services3-commons-node';
-// import { ConfigParams } from 'pip-services3-commons-node';
-// import { References } from 'pip-services3-commons-node';
-// import { ConsoleLogger } from 'pip-services3-components-node';
+import { Descriptor } from 'pip-services3-commons-node';
+import { ConfigParams } from 'pip-services3-commons-node';
+import { References } from 'pip-services3-commons-node';
+import { ConsoleLogger } from 'pip-services3-components-node';
 
-// import { BeaconsMemoryPersistence } from '../../../src/persistence/BeaconsMemoryPersistence';
-// import { BeaconsRestServiceV1 } from '../../../src/services/version1/BeaconsRestServiceV1';
-// import { BeaconsController } from '../../../src/logic/BeaconsController';
+import { BeaconsMemoryPersistence } from '../../../src/persistence/BeaconsMemoryPersistence';
+import { BeaconsRestServiceV1 } from '../../../src/services/version1/BeaconsRestServiceV1';
+import { BeaconsController } from '../../../src/logic/BeaconsController';
 
-// import { BeaconsClientV1Fixture } from './BeaconsClientV1Fixture';
-// import { BeaconsRestClientV1 } from '../../../src/clients/version1/BeaconsRestClientV1';
+import { BeaconsClientV1Fixture } from './BeaconsClientV1Fixture';
+import { BeaconsRestClientV1 } from '../../../src/clients/version1/BeaconsRestClientV1';
 
-// var httpConfig = ConfigParams.fromTuples(
-//     "connection.protocol", "http",
-//     "connection.host", "localhost",
-//     "connection.port", 3000
-// );
+var httpConfig = ConfigParams.fromTuples(
+    "connection.protocol", "http",
+    "connection.host", "localhost",
+    "connection.port", 3000
+);
 
-// suite('BeaconsRestClientV1', ()=> {
-//     let service: BeaconsRestServiceV1;
-//     let client: BeaconsRestClientV1;
-//     let fixture: BeaconsClientV1Fixture;
+suite('BeaconsRestClientV1', ()=> {
+    let service: BeaconsRestServiceV1;
+    let client: BeaconsRestClientV1;
+    let fixture: BeaconsClientV1Fixture;
 
-//     suiteSetup((done) => {
-//         let logger = new ConsoleLogger();
-//         let persistence = new BeaconsMemoryPersistence();
-//         let controller = new BeaconsController();
+    suiteSetup((done) => {
+        let logger = new ConsoleLogger();
+        let persistence = new BeaconsMemoryPersistence();
+        let controller = new BeaconsController();
 
-//         service = new BeaconsRestServiceV1();
-//         service.configure(httpConfig);
+        service = new BeaconsRestServiceV1();
+        service.configure(httpConfig);
 
-//         let references: References = References.fromTuples(
-//             new Descriptor('pip-services-commons', 'logger', 'console', 'default', '1.0'), logger,
-//             new Descriptor('beacons', 'persistence', 'memory', 'default', '1.0'), persistence,
-//             new Descriptor('beacons', 'controller', 'default', 'default', '1.0'), controller,
-//             new Descriptor('beacons', 'service', 'rest', 'default', '1.0'), service
-//         );
-//         controller.setReferences(references);
-//         service.setReferences(references);
+        let references: References = References.fromTuples(
+            new Descriptor('pip-services-commons', 'logger', 'console', 'default', '1.0'), logger,
+            new Descriptor('beacons', 'persistence', 'memory', 'default', '1.0'), persistence,
+            new Descriptor('beacons', 'controller', 'default', 'default', '1.0'), controller,
+            new Descriptor('beacons', 'service', 'rest', 'default', '1.0'), service
+        );
+        controller.setReferences(references);
+        service.setReferences(references);
 
-//         client = new BeaconsRestClientV1();
-//         client.setReferences(references);
-//         client.configure(httpConfig);
+        client = new BeaconsRestClientV1();
+        client.setReferences(references);
+        client.configure(httpConfig);
 
-//         fixture = new BeaconsClientV1Fixture(client);
+        fixture = new BeaconsClientV1Fixture(client);
 
-//         service.open(null, (err) => {
-//             client.open(null, done);
-//         });
-//     });
+        service.open(null, (err) => {
+            client.open(null, done);
+        });
+    });
 
-//     suiteTeardown((done) => {
-//         client.close(null);
-//         service.close(null, done);
-//     });
+    suiteTeardown((done) => {
+        client.close(null);
+        service.close(null, done);
+    });
     
-//     teardown((done) => {
-//         done();
-//     });
+    teardown((done) => {
+        done();
+    });
 
-//     test("CRUD Operations", (done) => {
-//         fixture.testCrudOperations(done);
-//     });
+    test("CRUD Operations", (done) => {
+        fixture.testCrudOperations(done);
+    });
 
-//     test("Calculate Positions", (done) => {
-//         fixture.testCalculatePosition(done);
-//     });
+    test("Calculate Positions", (done) => {
+        fixture.testCalculatePosition(done);
+    });
 
-// });
+});
